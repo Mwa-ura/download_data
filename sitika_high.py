efcb8f7ea0b32b8252c249117b04f8ca93a6414f
Mwa-ura <101228726+Mwa-ura@users.noreply.github.com>
2023-01-26 10:18:47 +0400

    Visualize high temp

--- sitika_high.py
+++ sitika_high.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 
 # Open csv file
 filename = 'data/sitka_weather_07-2018_simple.csv'
@@ -16,3 +17,15 @@ with open(filename) as f:
 		high_temp = int(temp[5])
 		high_temps.append(high_temp)
 print(high_temps)
+
+# Plot the high temp
+plt.style.use('seaborn')
+fix, ax = plt.subplots()
+ax.plot(high_temps, c="red")
+
+# Format graph
+ax.set_title("Sitka temperature")
+ax.set_xlabel("Time")
+ax.set_ylabel("High Temperature")
+plt.show()
+
