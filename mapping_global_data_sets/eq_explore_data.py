c08e7b27b6c3c693d0f4a470708e1d9f166d6e5b
Mwa-ura <101228726+Mwa-ura@users.noreply.github.com>
2023-02-10 09:29:04 +0400

    Extract mag data with for loop

--- mapping_global_data_sets/eq_explore_data.py
+++ mapping_global_data_sets/eq_explore_data.py
@@ -9,5 +9,11 @@ readable_data = 'data/readable_eq_data.json'
 with open(readable_data, 'w') as f:
 	json.dump(all_eq_data, f, indent=4)
 
-all_eq_dict = all_eq_data['features']
-print(len(all_eq_dict))
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))
+# Extract magnitude data
+mags = []
+for eq_dict in all_eq_dicts:
+	mag = eq_dict['properties']['mag']
+	mags.append(mag)
+print(mags[:10])
