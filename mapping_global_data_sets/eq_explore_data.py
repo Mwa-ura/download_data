41344efec1b58dcb6c07fd4472431d37bbecf9f2
Mwa-ura <101228726+Mwa-ura@users.noreply.github.com>
2023-02-23 09:50:43 +0400

    Refactor list append, automate header title and change data set

--- mapping_global_data_sets/eq_explore_data.py
+++ mapping_global_data_sets/eq_explore_data.py
@@ -3,7 +3,7 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 # Explore the structure of the file
-filename = 'data/eq_data_1_day_m1.json'
+filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f:
 	all_eq_data = json.load(f)
 
@@ -14,9 +14,10 @@ with open(readable_data, 'w') as f:
 all_eq_dicts = all_eq_data['features']
 print(len(all_eq_dicts))
 # Extract magnitude data
+"""
 mags, logs, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
-	mag = eq_dict['properties']['mag']
+	
 	log = eq_dict['geometry']['coordinates'][0]
 	lat = eq_dict['geometry']['coordinates'][1]
 	hover_text = eq_dict['properties']['title']
@@ -24,16 +25,18 @@ for eq_dict in all_eq_dicts:
 	logs.append(log)
 	lats.append(lat)
 	hover_texts.append(hover_text)
-# print(mags[:10])
-# print(logs[:10])
-# print(lats[:10])
+"""
+mags = [mag['properties']['mag'] for mag in all_eq_dicts]
+logs = [log['geometry']['coordinates'][0] for log in all_eq_dicts]
+lats = [lat['geometry']['coordinates'][1] for lat in all_eq_dicts]
+hover_texts = [hover_text['properties']['title'] for hover_text in all_eq_dicts]
 
 # Map the earthquakes.
 data = [{
 	'type':'scattergeo',
 	'lon':logs,
 	'lat':lats,
-	'text': hover_text,
+	'text': hover_texts,
 	'marker': {
 		'size': [5*mag for mag in mags],
 		'color': mags,
@@ -42,6 +45,9 @@ data = [{
 		'colorbar': {'title':'Magnitude'}
 		}
 	}]
-my_layout = Layout(title="Global Earthquakes")
+# Pull the metadata title
+header_title = all_eq_data['metadata']['title']
+print(header_title)
+my_layout = Layout(title=header_title)
 fig = {'data':data, 'layout':my_layout}
 offline.plot(fig, filename='global_earthquake.html')
